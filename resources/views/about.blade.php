a49891a6abd0b9c547fca406a681024a91aadf70
Victor Laguna <cvx.vlaguna@gmail.com>
2023-08-01 23:12:07 -0300

    section about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,12 @@
 @section('title', 'About')
 
 @section('content')
-
+<div>
+    <div class="bg-secondary">
+        <img src="{{ asset('/img/about.png') }}" class="w-full h-full pt-80" alt="about image"/>
+    </div>
+    <div class="bg-secondary">
+        <img src="{{ asset('/img/about.png') }}" class="w-full h-full pt-80" alt="about image"/>
+    </div>
+</div>
 @endsection
