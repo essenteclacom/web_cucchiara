99c44d3fdd1ac2415d627f47fbb5cec3a8b9766e
Victor Laguna <cvx.vlaguna@gmail.com>
2023-09-19 19:55:26 -0300

    newletter end and research module

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -635,8 +635,8 @@
                     Suscribite
                 </button>
                 <!-- Main modal -->
-                <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                    <div class="relative w-full max-w-md max-h-full">
+                <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="rounded fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative w-full max-w-2xl max-h-full">
                         <!-- Modal content -->
                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
@@ -645,41 +645,43 @@
                                 </svg>
                                 <span class="sr-only">Close modal</span>
                             </button>
-                            <div class="px-6 py-6 lg:px-8 bg-quarter">
-                                <h3 class="mb-4 text-xl font-medium text-white">Recibí nuestro Newsletter semanal</h3>
+                            <div class="px-6 py-20 bg-quarter flex flex-col items-center rounded">
+                                <h3 class="mb-4 text-2xl font-bold text-white">Recibí nuestro Newsletter semanal</h3>
                                 <form class="space-y-6" action="/home">
-                                    <div>
-                                        <div class="inline-block">
+                                    <div class="flex flex-row justify-between gap-3">
+                                        <div class="">
                                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 text-white"></label>
                                             <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nombres *" required>
                                         </div>
-                                        <div class="inline-block">
+                                        <div class="">
                                             <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 text-white"></label>
                                             <input type="lastname" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Apellidos *" required>
                                         </div>
                                     </div>
-                                    <div>
-                                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 text-white">Your email</label>
-                                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Email*" required>
+                                    <div class="w-full">
+                                        <input type="email" name="email" id="email" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Email*" required>
                                     </div>
                                     <div class="flex justify-between">
-                                        <div class="flex items-start">
-                                            <div class="flex items-center h-5">
-                                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                                        <div class="flex flex-col">
+                                            <div class="flex flex-row">
+                                                <div class="flex items-center h-5">
+                                                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                                                </div>
+                                                <label for="remember" class="ml-2 text-sm font-medium text-white">Resumen Estadistico</label><br>
                                             </div>
-                                            <label for="remember" class="ml-2 text-sm font-medium text-white">Resumen Estadistico</label><br>
-                                            <div class="flex items-center h-5">
-                                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                                            <div class="flex flex-row">
+                                                <div class="flex items-center h-5">
+                                                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                                                </div>
+                                                <label for="remember" class="ml-2 text-sm font-medium text-white">Resumen Economico</label>
                                             </div>
-                                            <label for="remember" class="ml-2 text-sm font-medium text-white">Resumen Economico</label>
                                         </div>
                                         <!--<a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>-->
                                     </div>
-                                    <div class="items-right justify-right">
-                                        <button type="submit" class="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar</button>
-                                    </div>
-                                    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                        Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                                    <div class="flex justify-end">
+                                        <button class="text-color-cucchiara rounded bg-cyan-20 px-12 py-2 font-body text-base font-bold transition-colors hover:bg-primary hover:text-white focus:border-transparent focus:outline-none focus:ring">
+                                            Enviar
+                                        </button>
                                     </div>
                                 </form>
                             </div>
